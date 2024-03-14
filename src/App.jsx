bf718a19cfe94c0d5e99caae430a64e48eabf143
Mariana Isaac <marian.isaac10@gmail.com>
2024-03-14 14:00:22 -0300

    sigo avanzando

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import { About } from "./component/About";
 import { Contact } from "./component/Contact";
+import { Navbar } from "./component/navbar/Navbar";
 import Projects from "./component/Projects";
 import { Skills } from "./component/Skills";
-import { Navbar } from "./component/navbar/Navbar.jsx";
 
 
 function App() {
   return (
-    <main className="text-gray-400 body-font">
-      <Navbar />
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar/>
       <About />
-      <Projects />
+      <Projects id="projects" />
       <Skills />
-      <Contact />
+      <Contact id="contact" />
     </main>
   );
 }
--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,54 +1,51 @@
 import { motion } from "framer-motion";
 
 export const About = () => {
+  const navigateToSection = (id) => {
+    const section = document.getElementById(id);
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  };
   return (
-    <section id="about" className="flex justify-center items-center">
-      <div className="mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <motion.div animate={{ x: 100 }} transition={{ delay: 1 }}>
-          <div className="lg:flex-grow lg:w-3/4 2xl:w-4/5 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white text-center flex flex-wrap justify-center items-center w-full">
-              <span className="w-full text-xl sm:text-2xl md:text-3xl lg:text-4xl">
-                PROGRAMADORA FRONT-END
-              </span>
-              <span className="w-full text-base sm:text-lg md:text-xl lg:text-2xl">
-                Mariana Isaac
-              </span>
-            </h1>
-            <p className="mb-8 leading-relaxed"></p>
-            <div className="flex justify-center w-full space-x-4">
-              <button
-                onClick={() => {
-                  /* función para navegar a #contact */
-                }}
-                className="inline-flex justify-center text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg flex-grow"
-              >
-                Contacto
-              </button>
-              <button
-                onClick={() => {
-                  /* función para navegar a #projects */
-                }}
-                className="inline-flex justify-center text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg flex-grow"
-              >
-                Mira mis trabajos
-              </button>
-            </div>
-          </div>
-        </motion.div>
-
-        <div className="container lg:max-w-lg lg:w-full md:w-1/2 w-5/6 flex justify-center">
-          <motion.img
-            className="object-cover object-center rounded-full mx-auto my-auto w-3/4 md:w-1/2"
-            alt="hero"
-            src="https://res.cloudinary.com/daupcpuqs/image/upload/v1706875738/Portafolio/CV_Mariana_Isaac_-_desarrolladora_ztxr0h.png"
-            animate={{
-              scale: [1, 2, 2, 1, 1],
-              rotate: [0, 0, 270, 270, 0],
-              borderRadius: ["50%", "20%", "20%", "20%", "50%"],
-            }}
-            transition={{ duration: 2 }}
-          />
-        </div>
+    <section id="about" className="flex justify-center items-center container">
+      <div className="container mx-auto flex py-20 md:flex-row flex-col justify-center items-center">
+        <div class="container lg:flex-grow lg:w-3/4 2xl:w-4/5 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center sm:items-center">
+  <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white text-center flex flex-wrap justify-center items-center w-full">
+    <span class="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+      PROGRAMADORA FRONT-END
+    </span>
+    <span class="w-full text-base sm:text-lg md:text-xl lg:text-2xl">
+      MARIANA ISAAC
+    </span>
+  </h1>
+  <p class="mb-8 leading-relaxed"></p>
+  <div class="container flex justify-center w-full space-x-4">
+  <button
+    onClick={() => navigateToSection('contact')}
+    class="inline-flex justify-center text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg flex-grow lg:w-auto md:w-1/2 sm:w-full"
+  >
+    Contacto
+  </button>
+  <button
+    onClick={() => navigateToSection('projects')}
+    class="inline-flex justify-center text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg flex-grow lg:w-auto md:w-1/2 sm:w-full"
+  >
+    Trabajos
+  </button>
+  </div>
+</div>
+<div className="container lg:max-w-lg lg:w-full md:w-1/2 w-5/6 flex justify-center">
+  <motion.img
+    className="object-cover object-center rounded-full mx-auto my-auto w-3/4 md:w-2/2 sm:w-2/4"
+    alt="hero"
+    src="https://res.cloudinary.com/daupcpuqs/image/upload/v1706875738/Portafolio/CV_Mariana_Isaac_-_desarrolladora_ztxr0h.png"
+    animate={{
+      scale: [1, 2, 2, 1, 1],
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["50%", "20%", "20%", "20%", "50%"],
+    }}
+    transition={{ duration: 2 }}
+  />
+</div>
       </div>
     </section>
   );
